caaceca2f0bff9be9b3c63188277b9962f73e5c6
Gopal Narayanan <gopastro@gmail.com>
2018-01-02 10:13:06 -0500

    Bug fix

--- msipbias/corba/biasmodule_corba_client.py
+++ msipbias/corba/biasmodule_corba_client.py
@@ -8,7 +8,7 @@ class BiasCorbaClient:
     def __init__(self):
         self.orb = CORBA.ORB_init(sys.argv, CORBA.ORB_ID)
         self.obj = self.orb.resolve_initial_references("NameService")
-        rootContext = obj._narrow(CosNaming.NamingContext)
+        rootContext = self.obj._narrow(CosNaming.NamingContext)
         if rootContext is None:
             print "Failed to narrow the root naming context"
             sys.exit(1)
@@ -17,12 +17,12 @@ class BiasCorbaClient:
         name = [CosNaming.NameComponent("test", "my_context"),
                 CosNaming.NameComponent("BiasModule", "Object")]
         try:
-            obj = rootContext.resolve(name)
+            self.obj = rootContext.resolve(name)
         except CosNaming.NamingContext.NotFound, ex:
             print "Name not found"
             sys.exit(1)
 
-        self.bo = obj._narrow(BiasCorba.BiasModuleCorba)
+        self.bo = self.obj._narrow(BiasCorba.BiasModuleCorba)
         if self.bo is None:
             print "Object reference is not an BiasCorba::BiasModuleCorba"
             sys.exit(1)
