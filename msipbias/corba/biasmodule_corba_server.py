bebe0a6557a083be6a44f1a7679697d28f0e3471
Gopal Narayanan <gopastro@gmail.com>
2018-01-01 23:16:44 -0500

    Make corba server a class

--- msipbias/corba/biasmodule_corba_server.py
+++ msipbias/corba/biasmodule_corba_server.py
@@ -19,47 +19,49 @@ class Bias_i (BiasCorba__POA.BiasModuleCorba):
         sensor = ((channel -1) % 3) + 1
         return self.bm.get_temperature(sensor=sensor, polar=polar)
 
-orb = CORBA.ORB_init(sys.argv, CORBA.ORB_ID)
-poa = orb.resolve_initial_references("RootPOA")
+class BiasModuleServer:
+    def __init__(self):
+        self.orb = CORBA.ORB_init(sys.argv, CORBA.ORB_ID)
+        self.poa = orb.resolve_initial_references("RootPOA")
 
-bi = Bias_i()
-bo = bi._this()
+        self.bi = Bias_i()
+        self.bo = self.bi._this()
 
-#Obtain a reference to the root naming context
-obj = orb.resolve_initial_references("NameService")
-rootContext = obj._narrow(CosNaming.NamingContext)
+        #Obtain a reference to the root naming context
+        obj = self.orb.resolve_initial_references("NameService")
+        rootContext = obj._narrow(CosNaming.NamingContext)
 
-if rootContext is None:
-    print "Failed to narrow the root naming context"
-    sys.exit(1)
+        if rootContext is None:
+            print "Failed to narrow the root naming context"
+            sys.exit(1)
 
-# Bind a context named "test.my_context" to the root context
-name = [CosNaming.NameComponent("test", "my_context")]
-try:
-    testContext = rootContext.bind_new_context(name)
-    print "New test context bound"
-except CosNaming.NamingContext.AlreadyBound, ex:
-    print "Test context already exists"
-    obj = rootContext.resolve(name)
-    testContext = obj._narrow(CosNaming.NamingContext)
-    if testContext is None:
-        print "test.mycontext exists but is not a NamingContext"
-        sys.exit(1)
+        # Bind a context named "test.my_context" to the root context
+        name = [CosNaming.NameComponent("test", "my_context")]
+        try:
+            testContext = rootContext.bind_new_context(name)
+            print "New test context bound"
+        except CosNaming.NamingContext.AlreadyBound, ex:
+            print "Test context already exists"
+            obj = rootContext.resolve(name)
+            testContext = obj._narrow(CosNaming.NamingContext)
+            if testContext is None:
+                print "test.mycontext exists but is not a NamingContext"
+                sys.exit(1)
 
-# Bind the Bias object to the test context
-name = [CosNaming.NameComponent("BiasModule", "Object")]
-try:
-    testContext.bind(name, bo)
-    print "New BiasModule object bound"
-except CosNaming.NamingContext.AlreadyBound:
-    testContext.rebind(name, bo)
-    print "BiasModule binding already existed -- rebound"
+        # Bind the Bias object to the test context
+        name = [CosNaming.NameComponent("BiasModule", "Object")]
+        try:
+            testContext.bind(name, bo)
+            print "New BiasModule object bound"
+        except CosNaming.NamingContext.AlreadyBound:
+            testContext.rebind(name, bo)
+            print "BiasModule binding already existed -- rebound"
 
-#Activate the poa
-poaManager = poa._get_the_POAManager()
-poaManager.activate()
-print orb.object_to_string(eo)
+        #Activate the poa
+        poaManager = self.poa._get_the_POAManager()
+        poaManager.activate()
+        print self.orb.object_to_string(eo)
 
-orb.run()
+        self.orb.run()
 
  
