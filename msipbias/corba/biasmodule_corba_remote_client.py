0032a9940b23a997b9092e6088e810126cc3d3d7
Gopal Narayanan <gopastro@gmail.com>
2018-01-02 12:54:10 -0500

    Add ability to read from a remote client

---
+++ msipbias/corba/biasmodule_corba_remote_client.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+
+import sys
+from omniORB import CORBA
+import BiasCorba, CosNaming
+
+class BiasCorbaRemoteClient:
+    def __init__(self, hostname='localhost'):
+        sys.argv.extend(("-ORBInitRef", "NameService=corbaname::%s" % hostname))
+        self.orb = CORBA.ORB_init(sys.argv, CORBA.ORB_ID)
+        self.obj = self.orb.resolve_initial_references("NameService")
+        rootContext = self.obj._narrow(CosNaming.NamingContext)
+        if rootContext is None:
+            print "Failed to narrow the root naming context"
+            sys.exit(1)
+
+        # Resolve the name "text.my_context/BiasModule.Object"
+        name = [CosNaming.NameComponent("test", "my_context"),
+                CosNaming.NameComponent("BiasModule", "Object")]
+        try:
+            self.obj = rootContext.resolve(name)
+        except CosNaming.NamingContext.NotFound, ex:
+            print "Name not found"
+            sys.exit(1)
+
+        self.bo = self.obj._narrow(BiasCorba.BiasModuleCorba)
+        if self.bo is None:
+            print "Object reference is not an BiasCorba::BiasModuleCorba"
+            sys.exit(1)
+
+    def getTemperature(self, channel):
+        temp = self.bo.getTemperature(channel)
+        return temp
