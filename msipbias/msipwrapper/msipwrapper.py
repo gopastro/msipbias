8242dcb11d0d581d9d413d249840a35d831506c4
Gopal Narayanan <gopastro@gmail.com>
2018-04-13 16:37:09 -0400

    Add an overall Wrapper for all msip1mm commands

---
+++ msipbias/msipwrapper/msipwrapper.py
@@ -0,0 +1,27 @@
+from msipbias.chopper import Chopper
+from msipbias.msiplo.msip_lo import MSIPLOSystem
+from msipbias.biasmodule import BiasModule
+
+class MSIPWrapper():
+    def __init__(self, debug=False):
+        self.debug = debug
+
+    def chopper_in(self):
+        chopper = Chopper(debug=self.debug)
+        chop = chopper.chopper_in()
+        chopper.close()
+        return chop
+
+    def chopper_out(self):
+        chopper = Chopper(debug=self.debug)
+        chop = chopper.chopper_out()
+        chopper.close()
+        return chop
+
+    def chopper_state(self):
+        chopper = Chopper(debug=self.debug)
+        chop = chopper.chopper_state()
+        chopper.close()
+        return chop
+
+    
